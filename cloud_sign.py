e645cc1d503027c7a9ce2f576f85f9711fae86a6
mkdir700 <mkdir700@gmail.com>
2020-03-18 09:42:43 +0800

    支持云函数执行

--- cloud_sign.py
+++ cloud_sign.py
@@ -3,7 +3,6 @@ import re
 import requests
 import asyncio
 import json
-import csv
 
 
 user_info = {
@@ -11,9 +10,6 @@ user_info = {
 	'password': '填写你的密码',
 	'学校id': '' # 学号登录才需要填写
 }
-
-
-csv_head = ['username', 'cookies']
 
 
 class AutoSign(object):
@@ -27,50 +23,61 @@ class AutoSign(object):
 			'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.100 Safari/537.36'}
 		self.session = requests.session()
 		# 读取指定用户的cookies
-		self.check_cookies_and_second_login(password, schoolid, username)
-		# self.login(password, schoolid, username)
-		self.save_cookies_to_csv(username)
+		if self.check_cookies_status(username) is False:
+			self.login(password, schoolid, username)
+			self.save_cookies(username)
 
-	def save_cookies_to_csv(self, username):
-		"""新cookies保存到csv文件中"""
-		row = [username, self.session.cookies.get_dict()]
-		with open('cookies.csv', 'a') as f:
-			f_csv = csv.writer(f)
-			# f_csv.writerow(csv_head)
-			f_csv.writerow(row)
+	def save_cookies(self, username):
+		"""保存cookies"""
+		new_cookies = self.session.cookies.get_dict()
+		with open("cookies.json", "r") as f:
+			data = json.load(f)
+			data[username] = new_cookies
+			with open("cookies.json", 'w') as f2:
+				json.dump(data, f2)
 
-	def check_cookies_and_second_login(self, password, schoolid, username):
-		"""检测csv文件内是否存有cookies,有则检测，无则登录"""
-		with open('cookies.csv', 'r') as f:
-			f_csv = csv.reader(f)
-			for u in f_csv:
-				# [username, cookies]
-				if username == u[0]:
-					# 设置cookies
-					self.session.cookies.update(u[1])
-					# 检测cookies是否有效
-					r = self.session.get('http://i.mooc.chaoxing.com/settings/info?t=1584461245774')
-					res = re.findall('<title>(.*)</title>', r.text)[0]
-					if res == "用户登录":
-						# 失效cookies
-						self.login(password, schoolid, username)
+	def check_cookies_status(self, username):
+		"""检测json文件内是否存有cookies,有则检测，无则登录"""
+		with open('cookies.json', 'r') as f:
+
+			# json文件有无账号cookies, 没有，则直接返回假
+			try:
+				data = json.load(f)
+				cookies = data[username]
+			except Exception:
+				return False
+
+			# 找到后设置cookies
+			for u in cookies:
+				self.session.cookies.set(u, cookies[u])
+
+			# 检测cookies是否有效
+			r = self.session.get('http://i.mooc.chaoxing.com/app/myapps.shtml', allow_redirects=False)
+			if r.status_code != 200:
+				print("cookies已失效，重新获取中")
+				return False
 			else:
-				self.login(password, schoolid, username)
-				self.save_cookies_to_csv(username)
+				print("cookies有效哦")
+				return True
 
 	def login(self, password, schoolid, username):
-
 		# 登录-手机邮箱登录
 		if schoolid:
 			r = self.session.post(
 				'http://passport2.chaoxing.com/api/login?name={}&pwd={}&schoolid={}&verify=0'.format(username, password, schoolid))
-			return
+			if json.loads(r.text)['result']:
+				print("登录成功")
+			else:
+				print("登录失败，请检查账号密码是否正确")
+
 		else:
 			r = self.session.get(
 				'https://passport2.chaoxing.com/api/login?name={}&pwd={}&schoolid=&verify=0'.format(username, password),
 				headers=self.headers)
-			return
-			# print(r.text)
+			if json.loads(r.text)['result']:
+				print("登录成功")
+			else:
+				print("登录失败，请检查账号密码是否正确")
 
 	def _get_all_classid(self) -> list:
 		"""获取课程主页中所有课程的classid和courseid"""
@@ -137,15 +144,16 @@ class AutoSign(object):
 		loop = asyncio.new_event_loop()
 		asyncio.set_event_loop(loop)
 		result = loop.run_until_complete(asyncio.gather(*tasks))
+		sign_msg = ""
 		for d in result:
 			if d is not None:
-				return '{}:{}'.format(d['classname'], self.sign_in(
+				sign_msg += '{}:{}'.format(d['classname'], self.sign_in(
 					d['classid'], d['courseid'], d['activeid']))
-		else:
-			return "当前暂无签到任务"
+		return sign_msg
 
 
 def main_handler(event=None, context=None):
+	"""腾讯云函数，执行这个方法"""
 	if "username" and "password" in event.keys():
 		# api请求
 		s = AutoSign(event['username'], event['password'])
@@ -156,12 +164,13 @@ def main_handler(event=None, context=None):
 	return result
 
 
-# def main_handler():
-# 	# 自身函数执行的时候，才执行这个，可用于定时任务
-# 	s = AutoSign(user_info['username'], user_info['password'])
-# 	result = s.run()
-# 	return result
+def local_run():
+	# 本地运行使用
+	s = AutoSign(user_info['username'], user_info['password'])
+	result = s.run()
+	print(result)
+	return result
 
 
 if __name__ == '__main__':
-	main_handler()
+	local_run()
